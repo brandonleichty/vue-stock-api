f3a11c32308e01f794569a5fdd8b17dbee0e690e
Brandon Leichty <brandonl@apple.com>
2022-05-11 20:03:08 -0500

    Add composition API example

--- src/App.vue
+++ src/App.vue
@@ -1,87 +1,50 @@
+<script setup>
+import CryptoSection from '@/components/Crypto-section.vue'
+import StockSection from '@/components/Stock-section.vue'
+import FooterArea from '@/components/UI/Footer-area.vue'
+import { ref } from 'vue'
+
+const component = ref(StockSection)
+const stockButtonStyle = ref('stocks-button')
+const cryptoButtonStyle = ref(null)
+
+function chooseComponent(cmp) {
+  component.value = cmp
+  if (component.value !== StockSection) {
+    document.body.classList.add('crypto-background')
+    stockButtonStyle.value = null
+    cryptoButtonStyle.value = 'crypto-button'
+  } else {
+    document.body.classList.remove('crypto-background')
+    stockButtonStyle.value = 'stocks-button'
+    cryptoButtonStyle.value = null
+  }
+}
+</script>
+
 <template>
   <div class="content-container">
     <div class="hero-container">
-      <h1 
-        v-if="component === 'StockSection'" 
-        class="stock-header">
-        Vue Stock App
-      </h1>
-      <h1 
-        v-else 
-        class="crypto-header">
-        Vue Crypto App
-      </h1>
+      <h1 v-if="component === 'StockSection'" class="stock-header">Vue Stock App</h1>
+      <h1 v-else class="crypto-header">Vue Crypto App</h1>
       <!-- NAVIGATION AREA TO SWTICH BETWEEN COMPONENTS -->
       <div class="tabs">
-        <button 
-          :class="stockButtonStyle" 
-          class="button stock-default" 
-          @click="chooseComponent('StockSection')">
+        <button :class="stockButtonStyle" class="button stock-default" @click="chooseComponent(StockSection)">
           Stocks
         </button>
-        <button 
-          :class="cryptoButtonStyle" 
-          class="button crypto-default" 
-          @click="chooseComponent('CryptoSection')">
+        <button :class="cryptoButtonStyle" class="button crypto-default" @click="chooseComponent(CryptoSection)">
           Crypto
         </button>
       </div>
       <keep-alive>
         <component :is="component"></component>
       </keep-alive>
-      <img 
-        v-if="component === 'StockSection'" class="crypto-logo" 
-        src="../src/images/stocks-logo.png" 
-        alt=""
-      >
-      <img 
-        v-else 
-        class="crypto-logo" 
-        src="../src/images/crypto-logo.png" 
-        alt=""
-      >
+      <img v-if="component === 'StockSection'" class="crypto-logo" src="../src/images/stocks-logo.png" alt="" />
+      <img v-else class="crypto-logo" src="../src/images/crypto-logo.png" alt="" />
     </div>
   </div>
   <FooterArea />
 </template>
-
-<script>
-import StockSection from '@/components/Stock-section.vue'
-import CryptoSection from '@/components/Crypto-section.vue'
-import FooterArea from '@/components/UI/Footer-area.vue'
-
-export default {
-  name: 'app',
-  components: {
-    // eslint-disable-next-line vue/no-unused-components
-    StockSection,
-    CryptoSection,
-    FooterArea
-  },
-  data () {
-    return {
-      component: 'StockSection',
-      stockButtonStyle: 'stocks-button',
-      cryptoButtonStyle: null,
-    }
-  },
-  computed: {},
-  methods: {
-    chooseComponent(cmp) {
-      this.component = cmp
-      if (this.component !== 'StockSection') {
-        document.body.classList.add('crypto-background')
-        this.stockButtonStyle = null
-        this.cryptoButtonStyle = 'crypto-button'
-      } else {
-        document.body.classList.remove('crypto-background')
-        this.stockButtonStyle = 'stocks-button'
-        this.cryptoButtonStyle = null
-      }
-    }
-  }
-}
-</script>
 
 <style>
 /* GLOBAL STYLES */
@@ -115,9 +78,9 @@ body {
 .stock-header {
   text-align: center;
   font-size: 82px;
-  font-family: 'Nunito', sans-serif; 
-  font-style: italic; 
-  background-image: linear-gradient(to right, #002d05 10%, #52E24B 90%);
+  font-family: 'Nunito', sans-serif;
+  font-style: italic;
+  background-image: linear-gradient(to right, #002d05 10%, #52e24b 90%);
   background-clip: text;
   -webkit-text-fill-color: transparent;
 }
@@ -125,8 +88,8 @@ body {
 .crypto-header {
   text-align: center;
   font-size: 82px;
-  font-family: 'Nunito', sans-serif; 
-  font-style: italic; 
+  font-family: 'Nunito', sans-serif;
+  font-style: italic;
   background-image: linear-gradient(to right, #00102d 10%, #1097f7 90%);
   background-clip: text;
   -webkit-text-fill-color: transparent;
@@ -155,13 +118,13 @@ body {
   border-top-left-radius: 10px;
   border: none;
   cursor: pointer;
-  transition: 0.2s ease
+  transition: 0.2s ease;
 }
 
 .stocks-button {
   margin-right: 0.5em;
   border-bottom: 2px solid;
-  border-image: linear-gradient(to right, #002F09, #52E24B) 1;   
+  border-image: linear-gradient(to right, #002f09, #52e24b) 1;
 }
 
 .crypto-button:hover {
@@ -174,28 +137,29 @@ body {
 
 .crypto-button {
   border-bottom: 2px solid;
-  border-image: linear-gradient(to right, #03254E, #1097f7) 1; 
+  border-image: linear-gradient(to right, #03254e, #1097f7) 1;
   border-bottom-right-radius: 10px;
 
   transition: 0.2s ease;
 }
 
 .crypto-button:active {
-  background-color: #03254E;
-  color: #E7F0FF;
+  background-color: #03254e;
+  color: #e7f0ff;
 }
 
 .crypto-default:hover {
   border-bottom: 2px solid;
-  border-image: linear-gradient(to right, #03254E, #1097f7) 1; 
+  border-image: linear-gradient(to right, #03254e, #1097f7) 1;
 }
 
 .stock-default:hover {
   border-bottom: 2px solid;
-  border-image: linear-gradient(to right, #002F09, #52E24B) 1;  
+  border-image: linear-gradient(to right, #002f09, #52e24b) 1;
 }
 
-.stocks-logo, .crypto-logo {
+.stocks-logo,
+.crypto-logo {
   opacity: 50%;
   position: fixed;
   width: 125px;
